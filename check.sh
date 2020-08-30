d295dabc58d1841d231af38211492f4d2b620fcb
Kenani Tadepalli <kenani101071@gmail.com>
2020-08-30 22:59:15 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -3,28 +3,26 @@
 #$IFS=$'\n' read -d '' -r -a lines < values.txt
 #status=${lines[0]}
 #currentpipe=${lines[1]}
-#status="BLOK"
-#currentpipe="pip2"
 echo $status
 echo $currentpipe
 
 if [ -z $status ] & [ -z $currentpipe ]
 then
     echo "NULL!! "
-    echo "Has dependency!!  Triggering.... Pipe1"
+    echo "Has dependency!!  Triggering.... pipe1"
 
         echo "Cancel current pipeline"
 		 exit 1
 
 else
     echo "Has value...! "
-    if [ $status = 'BLOK' ] && [ $currentpipe = 'pip2' ]
+    if [ $status = 'BLOCKED' ] && [ $currentpipe = 'pipe2' ]
     then
             echo "FOUND no dependency executing current Pipeline..."
 
 
     else
-            echo "Has dependency!!  Triggering.... Pipe1"
+            echo "Has dependency!!  Triggering.... pipe1"
 
             echo "Cancel current pipeline"
 				
