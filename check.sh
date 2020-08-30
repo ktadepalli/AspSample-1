af7e6ef8fdbb493dedfd820e6ee95f5d4a291fc8
Kenani Tadepalli <kenani101071@gmail.com>
2020-08-30 22:48:47 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-set -e
-sh -c
+
 #$IFS=$'\n' read -d '' -r -a lines < values.txt
 #status=${lines[0]}
 #currentpipe=${lines[1]}
@@ -18,7 +17,7 @@ then
 
 else
     echo "Has value...! "
-    if [ $status = 'BLOK' ] -a [ $currentpipe = 'pip3' ]
+    if [ $status = 'BLOK' ] && [ $currentpipe = 'pip3' ]
     then
             echo "Executing current Pipeline..."
 
