db47bc20160cec1f547636fe1bcd874d928ab3e4
Kenani Tadepalli <kenani101071@gmail.com>
2020-08-30 22:52:00 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -3,10 +3,10 @@
 #$IFS=$'\n' read -d '' -r -a lines < values.txt
 #status=${lines[0]}
 #currentpipe=${lines[1]}
-status="BLOK"
-currentpipe="pip2"
+#status="BLOK"
+#currentpipe="pip2"
 echo $status
-echo $currentpipes
+echo $currentpipe
 
 if [ -z $status ] & [ -z $currentpipe ]
 then
@@ -23,11 +23,11 @@ else
 
 
     else
-            echo "Condition not mactched.Triggering Pipe1"
+            echo "Has dependency!!  Triggering Pipe1"
 
-                echo "Cancel current pipeline"
+            echo "Cancel current pipeline"
 				
-				#exit 1
+	   exit 1
     fi
 
 fi
