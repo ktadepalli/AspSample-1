fc662e764e8adb9b71009190c7eebe1d823a9a75
Kenani Tadepalli <kenani101071@gmail.com>
2020-08-30 16:02:42 +0530

    Update conf.ps1

--- conf.ps1
+++ conf.ps1
@@ -1,1 +1,66 @@
 
+Param(
+    [Parameter(Mandatory=$true)]
+  
+    [String]
+    $status,
+
+    [Parameter(Mandatory=$true)]
+    [String]
+    $currentpipe,
+
+    [Parameter(Mandatory=$true)]
+    [String]
+    $BuildNO
+
+    )
+
+
+
+
+
+
+#PIPE2
+
+if ($status -eq 'NULL' -and $currentpipe -eq 'NULL')
+{
+  #CANCEL current pipeline
+
+
+  #TRIGGER Pipe 1
+  echo 'TRIGGER Pipe1'
+  
+
+}
+else
+{
+      if ($status -eq 'BLOCKED' -and $currentpipe -eq 'PIPE2')
+      {
+
+      #TRIGGER Pipe2
+
+      echo "TRIGGER Pipe2"
+
+      #UPDATE the status
+        
+      }
+      else
+      {
+
+        #CANCEL current pipeline
+
+        echo 'TRIGGER Pipe1'
+         #TRIGGER Pipe 1
+      
+      
+      }
+
+
+}
+
+
+
+
+
+
+
