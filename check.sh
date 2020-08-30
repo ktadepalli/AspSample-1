d1e6bd7433bf0d6421ff41a34236525f89467943
Kenani Tadepalli <kenani101071@gmail.com>
2020-08-30 20:08:07 +0530

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,25 @@
+
+IFS=$'\n' read -d '' -r -a lines < values.txt
+status=${lines[0]}
+currentpipe=${lines[1]}
+
+if [ -z $status ] & [ -z $currentpipe ]
+then
+    echo "Its NULL..... Trigger pipe1"
+
+        echo "Cancel current pipeline"
+
+else
+    echo "Has value...! $status: $currentpipe"
+        if [ $status == 'BLOCKED' ] & [ $currentpipe == 'pipe2' ]
+    then
+            echo "Executing current Pipeline..."
+
+
+    else
+            echo "Condition not mactched.Triggering Pipe1"
+
+                echo "Cancel current pipeline"
+    fi
+
+fi
