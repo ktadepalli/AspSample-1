d0c667a3ad137f434a2696590b1d416c45a2c74c
Kenani Tadepalli <kenani101071@gmail.com>
2020-08-31 12:22:05 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -10,7 +10,7 @@ if [ -z $status ] & [ -z $currentpipe ]
 then
     echo "NULL!! "
     echo "Has dependency!!  Triggering.... pipe1"
-    curl -X POST https://circleci.com/api/v1.1/project/github/ktadepalli/AspSample-1/build?branch=master -H "Circle-Token: 22df877158909c8e95b5f5e41e712a0a346d7ca2"
+    curl -X POST https://circleci.com/api/v1.1/project/github/ktadepalli/WinService-circle/build?branch=master -H "Circle-Token: 22df877158909c8e95b5f5e41e712a0a346d7ca2"
 
 
     echo "Cancel current pipeline"
@@ -26,7 +26,7 @@ else
 
     else
             echo "Has dependency!!  Triggering.... pipe1"
-	    curl -X POST https://circleci.com/api/v1.1/project/github/ktadepalli/AspSample-1/build?branch=master -H "Circle-Token: 22df877158909c8e95b5f5e41e712a0a346d7ca2"
+	    curl -X POST https://circleci.com/api/v1.1/project/github/ktadepalli/WinService-circle/build?branch=master -H "Circle-Token: 22df877158909c8e95b5f5e41e712a0a346d7ca2"
 
 
             echo "Cancel current pipeline"
